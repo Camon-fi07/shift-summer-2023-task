6e64c47439a2fef0a8d4a93bf82c9e6514a0e5f9
Camon-fi07 <422nikitos244@mail.ru>
2023-07-14 12:22:46 +0700

    улучшение дизайна

--- src/views/mainPage/ui/MainPage.tsx
+++ src/views/mainPage/ui/MainPage.tsx
@@ -3,14 +3,15 @@ import { Film } from "utils/types/film";
 import { pathToBack } from "utils/consts/pathToBack";
 import { useGetRequest } from "utils/hooks/useGetRequest";
 import { Load } from "components/load";
-import "./style.scss";
+import style from "./style.module.scss";
 
 export const MainPage = () => {
   const [films, setFilms] = useGetRequest<Film[]>(`${pathToBack}/cinema/today/`, "films");
   return (
-    <div className="main">
-      <div className="content">
-        <div className="films-list">
+    <div className={style.main}>
+      <div className={style.content}>
+        <h2 className={style.title}>Афиша</h2>
+        <div className={style.films_list}>
           {films === undefined ? (
             <Load />
           ) : (
